d80ebaf2967f4b188dcfef6c04422509edee95ab
Sun丶fire <1055951189@qq.com>
2024-04-10 22:33:54 +0800

    蓝点

--- app/src/main/java/com/smart/gaodemap/MainActivity.java
+++ app/src/main/java/com/smart/gaodemap/MainActivity.java
@@ -27,21 +27,22 @@ import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends Activity {
     MapView mMapView = null;
+    MyLocationStyle myLocationStyle;
     //初始化地图控制器对象
     AMap aMap;
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //声明定位回调监听器
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        ServiceSettings.updatePrivacyShow(this,true,true);
-//        ServiceSettings.updatePrivacyAgree(this,true);
-//        MyLocationStyle myLocationStyle;
-//        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-//        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
-//        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
-//        //aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
-//        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
 
+        ServiceSettings.updatePrivacyShow(this,true,true);
+        ServiceSettings.updatePrivacyAgree(this,true);
+
+     // 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
         setContentView(R.layout.activity_main);
         MapsInitializer.updatePrivacyShow(this,true,true);
         MapsInitializer.updatePrivacyAgree(this,true);
@@ -53,6 +54,27 @@ public class MainActivity extends Activity {
         if (aMap == null) {
             aMap = mMapView.getMap();
         }
+        AMapLocationListener mLocationListener = new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation aMapLocation) {
+
+            }
+        };
+        //初始化定位
+        try {
+            mLocationClient = new AMapLocationClient(getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mLocationListener);
+
+        //定位蓝点//
+        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
+        aMap.setMyLocationEnabled(true);
     }
     @Override
     protected void onDestroy() {
